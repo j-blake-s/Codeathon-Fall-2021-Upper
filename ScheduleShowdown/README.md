b1395a62ed33aba3feb88f0d25d81184ee11af2b
Charles Daniels <charles@cdaniels.net>
2021-10-11 22:11:51 -0400

    add examples and brute-force solution

--- ScheduleShowdown/README.md
+++ ScheduleShowdown/README.md
@@ -8,7 +8,9 @@ classes this semester must have the harder degree.
 Help Alice and Bob settle their argument by determining the schedule for each
 of them which contains the most classes, then seeing which schedule has the
 greater number classes. Note that the Registrar will not allow students to
-register for classes which occur at overlapping times.
+register for classes which occur at overlapping times, however the registrar
+does not account for travel time, so a student *can* register for a class which
+ends exactly at the same time as another which they are registered for begins.
 
 ## Input
 
@@ -47,7 +49,85 @@ on who has the busiest possible schedule:
 The second line of your output should be the number of classes the busier
 person has in their schedule.
 
-## Example
+## Examples
 
-TODO
+### Example 1
+
+Input:
+
+```
+3
+07:00 08:10
+09:00 09:50
+10:00 12:30
+2
+08:00 09:00
+10:00 11:00
+```
+
+Output:
+
+```
+Alice
+3
+```
+
+Explanation:
+
+Alice has 3 possible classes she could register for. Since none of them
+overlap, her busiest possible schedule would be to register for all three
+classes.
+
+
+Bob has 2 possible classes he could register for, neither of which overlap.
+Even though he can take all of them, his busiest schedule is only 2 classes.
+
+3 is more than 2, so Alice must have the more difficult major, hence our output
+is `Alice`, followed by `3`, the number of classes in her busiest schedule.
+
+### Example 2
+
+Input:
+
+```
+5
+08:00 09:15
+09:00 10:15
+10:30 11:30
+14:00 15:15
+16:00 16:45
+6
+11:00 11:45
+11:30 12:30
+11:55 13:00
+12:30 14:30
+16:00 18:00
+18:30 20:00
+```
+
+Output:
+
+```
+Tie
+4
+```
+
+Explanation:
+
+Alice has 5 possible classes she could register for, but some of them overlap.
+She must choose between the `09:00 10:15` class and the `9:45 11:30` class,
+since the latter starts before the former ends. Alice has two busiest possible
+schedules, being:
+* `08:00 09:15`, `10:30 11:30`, `14:00 15:15`, `16:00 16:45`
+* `09:00 10:15`, `10:30 11:30`, `14:00 15:15`, `16:00 16:45`
+
+
+
+Bob has 6 possible classes, but two of them overlap: he can't take both the
+`11:00 11:45` and the `11:30 12:30` class, nor can he take both the `11:55
+13:00` and `12:30 14:30` classes at once. Bob has three possible busiest
+schedules, each with four classes:
+* `11:00 11:45`, `11:55 13:00`, `16:00 18:00`, `18:30 20:00`
+* `11:00 11:45`, `12:30 14:30`, `16:00 18:00`, `18:30 20:00`
+* `11:30 12:30`, `12:30 14:30`, `16:00 18:00`, `18:30 20:00`
 
---
+++ ScheduleShowdown/solutions/bruteforce.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Charles Daniels
+
+# Brute force solution to ScheduleShowdown. Runs in O((A+B)^2). Will break for
+# inputs where A or B are more than about 64 or so since it uses an int to
+# store the bit vector for what schedule indices to include or not.
+
+import sys
+
+state = 0  # 0 - reading A, 1 - reading Alice's times, 2 - reading B, 3 - reading Bob's times
+
+A = None
+B = None
+atimes = []
+btimes = []
+
+def fmtS(s):
+    M = int((s / 60) % 60)
+    H = int((s / 60) / 60)
+    return "{:02d}:{:02d}".format(H, M)
+
+for line in sys.stdin:
+    if state == 0:
+        A = int(line)
+        state = 1
+
+    elif state == 2:
+        B = int(line)
+        state = 3
+
+    elif state in [1,3]:
+        start, end = line.strip().split()
+        startH, startM = [int(x) for x in start.split(":")]
+        endH, endM = [int(x) for x in end.split(":")]
+        startS = startH * 60 * 60 + startM * 60
+        endS = endH * 60 * 60 + endM * 60
+
+        if state == 1:
+            atimes.append((startS, endS))
+        elif state == 3:
+            btimes.append((startS, endS))
+
+        if (state == 1) and (len(atimes) >= A):
+            state = 2
+
+def bvsubset(bv, lst):
+    # Given a bit-vector bv, and a list lst, return a new list consisting of
+    # only elements lst[i] where bv & (1 << i) is True.
+
+    res = []
+    for i in range(len(lst)):
+        if (bv & (1 << i)) > 0:
+            res.append(lst[i])
+    return res
+
+def checksched(s):
+    # Given a schedule s of tuples (start, end) in seconds, return whether
+    # or not the schedule is valid.
+
+    for i in range(len(s)):
+        for j in range(len(s)):
+            if i == j:
+                continue
+
+            # There are 3 cases (note that every class will take a turn being i
+            # and j, since we are doing this naively).
+            #
+            # Both classes occur at the same time
+            # |--- i ---|
+            # |--- j ---|
+            #
+            # i starts first, but ends after j starts
+            # |--- i ---|
+            #    |--- j ---|
+            #
+            # j starts and ends within the running time of j
+            # |------ i ------|
+            #    |--- j ---|
+            #
+            # These cases are OK:
+            #
+            # |--- i ---|
+            #               |--- j ---|
+            #
+            #               |--- i ---|
+            # |--- j ---|
+            #
+            # We can easily check if a class-pair conflicts by simply seeing if
+            # either the start time or end time of j falls within the bounds of
+            # i.
+
+            # start time of i falls within j
+            if (s[i][0] < s[j][1]) and (s[i][0] > s[j][0]):
+                return False
+
+            # end time of i falls within j
+            if (s[i][1] < s[j][1]) and (s[i][1] > s[j][0]):
+                return False
+
+            # i and j are at exactly the same time
+            if (s[i][0] == s[j][0]) and (s[i][1] == s[j][1]):
+                return False
+
+    return True
+
+def sched2str(s):
+    return ", ".join([fmtS(t[0]) + " " + fmtS(t[1]) for t in s])
+
+amax = 0
+bmax = 0
+
+print("Alice: ")
+for abv in range(pow(2, len(atimes))):
+    asched = bvsubset(abv, atimes)
+    if not checksched(asched):
+        # print("reject: {}: {}".format(len(asched), sched2str(asched)))
+        continue
+    print("{}: {}".format(len(asched), sched2str(asched)))
+
+    if len(asched) > amax:
+        amax = len(asched)
+
+print("Bob: ")
+for bbv in range(pow(2, len(btimes))):
+    bsched = bvsubset(bbv, btimes)
+    if not checksched(bsched):
+        # print("reject: {}: {}".format(len(asched), sched2str(asched)))
+        continue
+    print("{}: {}".format(len(bsched), sched2str(bsched)))
+
+    if len(bsched) > bmax:
+        bmax = len(bsched)
+
+print("Alice: {}, Bob: {}".format(amax, bmax))
